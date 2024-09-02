5a82e0f5f8bc0f1c0f322ba043004e00378d6eb2
Asif <107271220+Asif2902@users.noreply.github.com>
2024-09-02 07:08:05 +0600

    update api/saveData.js

--- api/saveData.js
+++ api/saveData.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+// Create the user_data directory if it doesn't exist
+const userDataDir = path.join(__dirname, 'user_data');
+if (!fs.existsSync(userDataDir)) {
+    fs.mkdirSync(userDataDir);
+}
+
+// Endpoint to save user data
 app.post('/save-data', (req, res) => {
     const { tgUserId, balance, energy, walletAddress } = req.body;
 
     const data = `User ID: ${tgUserId}\nBalance: ${balance}\nEnergy: ${energy}\nWallet: ${walletAddress}\n`;
     
-    fs.writeFile(`./user_data/${tgUserId}.txt`, data, (err) => {
+    const filePath = path.join(userDataDir, `${tgUserId}.txt`);
+
+    fs.writeFile(filePath, data, (err) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Error saving data');
